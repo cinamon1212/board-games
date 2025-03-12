63a4190b33b87c219b4e137ce49cc947e98ec264
Kuzmin Andrey <andrey.kuzmin.2806@mail.ru>
2025-03-12 23:06:12 +0300

    стартовый коммит с настроенными prettier и eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['plugin:@typescript-eslint/recommended', 'next/core-web-vitals', 'plugin:prettier/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
+    },
+    plugins: ['@typescript-eslint', '@next/next', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
