fcc1d36099c84201b351eb3a6132b27339c3c388
Kuzmin Andrey <andrey.kuzmin.2806@mail.ru>
2025-05-10 13:31:59 +0300

    fix: useWindowWidth

--- src/hooks/useWindowWidth.ts
+++ src/hooks/useWindowWidth.ts
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 
 export const useWindowWidth = () => {
-  const [width, setWidth] = useState(window.innerWidth)
+  const [width, setWidth] = useState(0)
 
   useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth)
+    const handleResize = () => setWidth(window?.innerWidth)
 
     window.addEventListener('resize', handleResize)
+
+    handleResize()
 
     return () => window.removeEventListener('resize', handleResize)
   }, [])
