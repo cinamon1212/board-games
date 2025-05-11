72c23fca639e86485176a377b02153e9e0944741
Ilya K <ilia.kuzmin.01.01.01.01@mail.ru>
2025-05-11 13:14:00 +0300

    Обновил стили и логику input-search

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { ChangeEventHandler } from 'react'
-import { CloseIcon, iconStyles, Input, SearchIcon, SearchInputWrapper } from './styles'
+import { CloseIcon, Input, SearchIcon, SearchInputWrapper } from './styles'
 
 type SearchInputProps = {
   setInputValue: React.Dispatch<React.SetStateAction<string>>
@@ -13,9 +13,9 @@ export const SearchInput = ({ setInputValue, inputValue }: SearchInputProps) => 
 
   return (
     <SearchInputWrapper>
-      <SearchIcon src='/search-icon.png' alt='search' {...iconStyles} />
+      <SearchIcon src='/search-icon.png' alt='search' />
       <Input placeholder='Поиск' value={inputValue} onChange={onChange} />
-      <CloseIcon src='/close-icon.png' alt='close' {...iconStyles} onClick={onClose} />
+      {inputValue && <CloseIcon src='/close-icon.png' alt='close' onClick={onClose} />}
     </SearchInputWrapper>
   )
 }
--- src/components/SearchInput/styles.ts
+++ src/components/SearchInput/styles.ts
@@ -1,46 +1,49 @@
 import styled from 'styled-components'
 
 export const SearchInputWrapper = styled.section`
-  border-radius: 16px;
+  width: 300px;
   height: 42px;
   display: flex;
   align-items: center;
-  background-color: #474746;
-  width: 300px;
-  gap: 12px;
-  padding: 0 12px;
-  box-shadow: #474746 -4px 5px 15px -6px;
-  border: none;
-  transition: box-shadow 300ms ease;
-
-  &:hover,
-  &:focus {
-    box-shadow: #735b44 -4px 5px 15px -6px;
-  }
+  position: relative;
 `
 
 export const Input = styled.input`
-  height: 100%;
-  border: none;
-  outline: none;
-  background: inherit;
+  font-family: 'Geist Mono', 'Geist Mono Fallback';
   width: 100%;
+  border-radius: 16px;
+  background-color: #474746;
+  padding: 12px 34px;
+  box-shadow: #474746 -4px 9px 25px -6px;
+  border: none;
+  transition: box-shadow 300ms ease;
   color: whitesmoke;
   font-size: 16px;
 
   &::placeholder {
     color: #858585;
   }
+
+  &:hover,
+  &:focus {
+    box-shadow: #735b44 -4px 9px 25px -6px;
+    outline: none;
+  }
 `
 
 export const SearchIcon = styled.img`
-  height: 50%;
+  position: absolute;
+  left: 5px;
+  width: 21px;
+  height: 21px;
 `
 export const CloseIcon = styled.img`
-  height: 50%;
+  position: absolute;
+  width: 21px;
+  height: 21px;
+  right: 5px;
 
   &:hover {
     cursor: pointer;
   }
 `
-export const iconStyles = { height: 20, width: 25 }
