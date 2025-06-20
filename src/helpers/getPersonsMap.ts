1fc216f6f10e01e12eb03cc6136ecb4738dea4ee
Kuzmin Andrey <andrey.kuzmin.2806@mail.ru>
2025-06-20 21:25:30 +0300

    fix: убрал лишние labels в gamesForChart

--- src/helpers/getPersonsMap.ts
+++ src/helpers/getPersonsMap.ts
@@ -1,34 +1,38 @@
-import { PersonsMap, PersonsMapGames, Player } from "@/types"
+import { PersonsMap, PersonsMapGames, Player } from '@/types'
 
-import { SingleGameResult } from '../types';
+import { SingleGameResult } from '../types'
 
-import { getArithmeticMean } from "./getArithmeticMean";
+import { getArithmeticMean } from './getArithmeticMean'
 
 export const getPersonsMap = <T extends SingleGameResult>(map: PersonsMapGames<T>) => {
-    const personsMap: PersonsMap<T> = {}
-    let idx = 0
-  
-    for (const person in map) {
-      const name = person as Player
-  
-      if (map[name]) {
-        const {scores, winCount, ...fields} = map[name]
-        const isBoolean = typeof scores[0] === 'boolean'
-        const numScores = scores as Array<number>
-  
-        personsMap[name] = {
-          scores,
-          scoresCount: scores.length,
-          avg: isBoolean ? undefined : getArithmeticMean(numScores),
-          minScore: isBoolean ? undefined : Math.min(...numScores),
-          maxScore: isBoolean ? undefined : Math.max(...numScores),
-          winRate: `${Math.ceil((winCount / scores.length) * 100)}%`,
-          ...fields
-        }
+  const personsMap: PersonsMap<T> = {}
+  let idx = 0
+
+  let maxScoresCount = 0
+
+  for (const person in map) {
+    const name = person as Player
+
+    if (map[name]) {
+      const { scores, winCount, ...fields } = map[name]
+      const isBoolean = typeof scores[0] === 'boolean'
+      const numScores = scores as Array<number>
+
+      personsMap[name] = {
+        scores,
+        scoresCount: scores.length,
+        avg: isBoolean ? undefined : getArithmeticMean(numScores),
+        minScore: isBoolean ? undefined : Math.min(...numScores),
+        maxScore: isBoolean ? undefined : Math.max(...numScores),
+        winRate: `${Math.ceil((winCount / scores.length) * 100)}%`,
+        ...fields,
       }
-  
-      idx++
+
+      if (scores.length > maxScoresCount) maxScoresCount = scores.length
     }
 
-    return personsMap
+    idx++
+  }
+
+  return { personsMap, maxScoresCount }
 }
