60711f7e714b75b5f5d9b54940f67876ee3ddb65
Kuzmin Andrey <andrey.kuzmin.2806@mail.ru>
2025-06-20 21:44:00 +0300

    fix: getTransformedDataFromBoolGames

--- src/helpers/getTransformedDataFromBoolGames.ts
+++ src/helpers/getTransformedDataFromBoolGames.ts
@@ -7,7 +7,8 @@ import { transformDataForTable } from './transformDataForTable'
 
 export const getTransformedDataFromBoolGames = (boolGames: PlayerScores<boolean>) => {
   const map = getPersonsMapGames<boolean>(boolGames)
-  const personsMap = getPersonsMap<boolean>(map)
+  
+  const { personsMap } = getPersonsMap<boolean>(map)
 
   const data = createDataForPie(personsMap)
   const tableDataArr = transformDataForTable(personsMap, 'winRate')
