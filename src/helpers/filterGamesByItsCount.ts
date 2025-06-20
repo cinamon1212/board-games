a9159e53728685d0940e80686a2ab98de913a1e6
Kuzmin Andrey <andrey.kuzmin.2806@mail.ru>
2025-06-20 22:04:07 +0300

    fix: добавил сортировку игр по количеству сыгранных матчей

--- src/helpers/filterGamesByItsCount.ts
+++ src/helpers/filterGamesByItsCount.ts
@@ -1,3 +1,6 @@
 import { Games } from '@/types'
 
-export const filterGamesByItsCount = (games: Games) => games.filter(({ games: gamesList }) => gamesList.length)
+export const filterGamesByItsCount = (games: Games) =>
+  games
+    .filter(({ games: gamesList }) => gamesList.length)
+    .sort((firstGame, secondGame) => (firstGame.games.length < secondGame.games.length ? 1 : -1))
