bd9b435ccb33c92c9d6faa8e35198feefab32794
Ilya K <ilia.kuzmin.01.01.01.01@mail.ru>
2025-06-22 12:49:05 +0300

    Обновил таблицу

--- src/components/Table/styles.ts
+++ src/components/Table/styles.ts
@@ -1,17 +1,34 @@
 import { BREAK_POINTS } from '@/constants'
 import styled, { CSSProperties } from 'styled-components'
 
+export const TableRow = styled.tr<{
+  $hoverBgColor?: CSSProperties['backgroundColor']
+}>`
+  tbody & {
+    &:hover td {
+      background-color: ${(props) => props.$hoverBgColor};
+      transition: all 0.3s ease-in-out;
+    }
+  }
+  thead & {
+    th:hover {
+      background-color: ${(props) => props.$hoverBgColor};
+      transition: all 0.3s ease-in-out;
+    }
+  }
+`
+
 export const TableCell = styled.td<{
   $hoverBgColor?: CSSProperties['backgroundColor']
   $bgColor?: CSSProperties['backgroundColor']
   $color?: CSSProperties['color']
 }>`
-  border: 0.5px solid #575553;
+  border-bottom: 0.5px solid #57555380;
+  border-right: 0.5px solid #57555380;
   padding: 12px;
   text-align: center;
   width: fit-content;
   position: relative;
-  transition: all 0.3s ease-in-out;
 
   ${({ $color }) =>
     $color &&
@@ -23,23 +40,21 @@ export const TableCell = styled.td<{
       background-clip: text;
       color: transparent;
     `}
-
+  color: ${(props) => props.color};
   background-color: ${(props) => props.$bgColor};
-
-  &:hover {
-    background-color: ${(props) => props.$hoverBgColor};
-  }
 
   @media (max-width: ${BREAK_POINTS.sm}) {
     padding: 8px;
   }
 `
+
 export const TableStyled = styled.table`
   border-collapse: separate;
   width: 100%;
   overflow: hidden;
   min-width: 400px;
   margin: 0 auto;
+  border-spacing: 0;
 
   @media (max-width: ${BREAK_POINTS.sm}) {
     font-size: 12px;
@@ -49,12 +64,16 @@ export const TableStyled = styled.table`
 `
 
 export const TableWrapper = styled.div`
-  overflow-x: scroll;
+  overflow: hidden;
   max-width: 100%;
   width: 100%;
-  border: 0.5px solid #575553;
+  border: 0.5px solid #575553cc;
   border-radius: 12px;
   scrollbar-width: thin;
+
+  @media (max-width: ${BREAK_POINTS.sm}) {
+    overflow-x: scroll;
+  }
 `
 export const SortIcon = styled.span`
   position: absolute;
